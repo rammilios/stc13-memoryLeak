2f232580a5f6044ac735275b1813ffe121b4ae79
rammilios <ram7260@yandex.ru>
2018-09-24 18:57:10 +0300

    initial commit

---
+++ src/memoryLeak/Main.java
@@ -0,0 +1,39 @@
+package memoryLeak;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Main {
+
+    static class Key {
+        Integer id;
+
+        Key(Integer id) {
+            this.id = id;
+        }
+
+//        Solution
+//        @Override
+//        public boolean equals(Object o) {
+//            boolean response = false;
+//            if (o instanceof Key) {
+//                response = (((Key) o).id).equals(this.id);
+//            }
+//            return response;
+//
+//        }
+
+        @Override
+        public int hashCode() {
+            return id.hashCode();
+        }
+    }
+
+    public static void main(String[] args) {
+        Map<Key, String> m = new HashMap<Key, String>();
+        while (true)
+            for (int i = 0; i < 10000; i++)
+                if (!m.containsKey(new Key(i)))
+                    m.put(new Key(i), "Number:" + i);
+    }
+}
